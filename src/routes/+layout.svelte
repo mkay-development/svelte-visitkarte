30007b7fc8eb94a330fecbd198b0da7b87fd112d
Jonathan Martz <git@jmartz.de>
2023-01-30 16:59:11 +0100

    add content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <header class="mx-auto max-w-5xl my-2 flex justify-between px-2 py-2">
-  <h1 class="text-xl font-bold"><a href="/">Logo</a></h1>
+  <h1 class="text-xl font-bold"><a href="/">MD - Visitenkarte</a></h1>
   <nav>Test</nav>
 </header>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,33 @@
-<h2>Willkommen auf der Seite der Mail.app :D</h2>
+<script></script>
+
+<section class="w-96 h-48 mx-auto mt-12 bg-[#324A5F] px-3 py-3">
+  <div class="grid grid-cols-6">
+    <div class="col-span-2 border-r border-white mt-4 pb-6">
+      <img
+        src="https://avatars.githubusercontent.com/u/20771653?v=4"
+        class="rounded-full mx-auto mt-4 w-24"
+        alt=""
+      />
+    </div>
+    <div class="col-span-4 text-white">
+      <h2 class="font-bold mt-14 text-center">Jonathan Martz</h2>
+      <p class="text-sm text-center mt-1">Geschäftsführer & Entwickler</p>
+    </div>
+  </div>
+</section>
+
+<section class="w-96 h-48 mx-auto mt-12 bg-[#324A5F] px-3 py-3 text-white">
+  <div class="logo flex justify-center mt-3">
+    <div class="logo-klammer text-[#1f6fed] text-4xl mr-3">&#123;</div>
+    <div class="logo-text">
+      <span>MkaY Development</span> <br />
+      <span class="text-sm text-gray-400">UG haftungsbeschränkt</span>
+    </div>
+    <div class="logo-klammer text-[#1f6fed] text-4xl ml-3">&#125;</div>
+  </div>
+  <p class="block text-center mt-3 text-sm">
+    Othestrasse 2 <br>
+    51702 Bergneustadt <br>
+    02261 / 58639
+  </p>
+</section>
